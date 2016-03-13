b32ca621be5c444793e83a70e8137fc41b3fa2de
Lachezar Lechev <elpiel93@gmail.com>
2016-03-13 09:39:42 +0200

    1) Add mirage with dummy data 2) Rental Model 3) [Index] Use model

---
+++ app/mirage/scenarios/default-compiled-compiled.js
@@ -0,0 +1,11 @@
+export default function () /* server */{}
+
+// Seed your development database using your factories. This
+// data will not be loaded in your tests.
+
+// server.createList('contact', 10);
+
+
+//# sourceMappingURL=default-compiled.js.map
+
+//# sourceMappingURL=default-compiled-compiled.js.map
--- app/models/rental-compiled.js
+++ app/models/rental-compiled.js
@@ -1,5 +1,12 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({});
+export default DS.Model.extend({
+  title: DS.attr(),
+  owner: DS.attr(),
+  city: DS.attr(),
+  type: DS.attr(),
+  image: DS.attr(),
+  bedrooms: DS.attr()
+});
 
 //# sourceMappingURL=rental-compiled.js.map
